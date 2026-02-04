6a73d903df13904ac451ab70e7d36a821142dd12
sokhnag <sm.gueye.2025@alumnos.urjc.es>
2026-02-04 17:53:26 +0100

    Fibonacci

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -11,6 +11,6 @@ def fibon(n):
         i += 1
     return lst[-1]
 
-print("The fifth term of the Fibonacci sequence is:  " , fibon(5))
+print("The fifth term of the Fibonacci sequence is:   " , fibon(5))
 print("The tenth term of the Fibonacci sequence is:  " , fibon(10))
 print("The fifteenth term of the Fibonacci sequence is:  " , fibon(15))
