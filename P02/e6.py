e6a070c46e70688868bf2a6433a450d1f6a349c1
sokhnag <sm.gueye.2025@alumnos.urjc.es>
2026-03-10 14:53:33 +0100

    .

--- P02/e6.py
+++ P02/e6.py
@@ -10,6 +10,7 @@ print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 IP = "212.128.255.74"
 PORT1 = 8080
+
 PORT2 = 8081
 
 c = Client(IP, PORT1)
@@ -21,10 +22,11 @@ b = str(s.read_fasta("sequences/FRAT1.txt"))
 n = [c, c2]
 for j in n:
     print(j)
-    j.talk("Sending FRAT1 gene to the server, in fragments of 10 bases...")
+    j.talk("Sending FRAT1 gene to the server, in fragments of 10 bases...  ")
 
 print(f"Gene FRAT1: {b}")
 fragments = 1
+
 f = ""
 for a in b:
     if fragments <= 10:
